e9e99378d4f82c5a4598ee31a83db603c5ed8f00
Ajay Singh <aks0710h@insurancetechnologies.com>
2025-11-17 20:53:53 +0530

    ajay testing

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test2 HMR
+          Edit <code>src/App.jsx</code> and save to test3 HMR
         </p>
       </div>
       <p className="read-the-docs">
